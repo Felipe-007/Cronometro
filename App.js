575673b193c0bfb9c10cdcd7da6362bfec54feba
Felipe <felipe.presidencia@gmail.com>
2022-06-19 21:51:20 -0300

    C1 - App.js

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function App(){
   return(
     <View style={styles.container}>
-      <Text>Felipe Lopes</Text>
+
+      <Image
+        source={require('./src/crono.png')}
+      />
+
+      <Text style={styles.timer}>00:00:00</Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>Iniciar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}>Ultima marcação 00:00:30</Text>
+      </View>
+
     </View>
   );
 }
@@ -14,5 +34,39 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  }
+    backgroundColor: '#00aeef'
+  },
+  timer:{
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40,
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9,
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+  areaUltima:{
+    marginTop: 40,
+  },
+  textoCorrida:{
+    fontSize: 23,
+    color: '#FFF',
+    fontStyle: 'italic',
+  },
 })
