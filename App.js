eb7dbe6c7c6e89821abed1ca6ff6e57a0723e6fa
Felipe <felipe.presidencia@gmail.com>
2022-06-27 23:16:45 -0300

    C3 - App.js, styles.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { styles } from "./src/Home/styles"; 
 
 let timer = null;
 let ss = 0;
@@ -89,45 +90,3 @@ export default function App(){
     </View>// { ultimo ? 'Ultimo tempo: ' + ultimo: '' } caso o ultimo esteja vazio, não apresentará nada
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#00aeef'
-  },
-  timer:{
-    marginTop: -160,
-    fontSize: 45,
-    fontWeight: 'bold',
-    color: '#FFF',
-  },
-  btnArea:{
-    flexDirection: 'row',
-    marginTop: 130,
-    height: 40,
-  },
-  btn:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-    height: 40,
-    margin: 17,
-    borderRadius: 9,
-  },
-  btnTexto:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#00aeef'
-  },
-  areaUltima:{
-    marginTop: 40,
-  },
-  textoCorrida:{
-    fontSize: 23,
-    color: '#FFF',
-    fontStyle: 'italic',
-  },
-})
